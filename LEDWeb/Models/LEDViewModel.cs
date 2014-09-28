debe75183ef35c4cb30aded229abdf53ec4fb78c
mark.heaver <mark.heaver@tombola.com>
2014-09-28 18:11:49 +0100

    adding some home controller functionality plus starting validation

---
+++ LEDWeb/Models/LEDViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Services;
+
+namespace LEDWeb.Models {
+	public class LEDViewModel 
+	{
+		[WebValidation]
+		public string UserInput { get; set; }
+	}
+}
---
+++ Services/WebValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Services {
+	public class WebValidation
+	{
+		private readonly InputHandler _inputHandler = new InputHandler();
+	//	public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata,ControllerContext context) {
+	//	throw new NotImplementedException();
+	//	}
+
+	//	public override bool ValidationResult
+	//	protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+	//	{
+	//		if (!_inputHandler.IsInputValid(value.ToString()))
+	//		{
+	//			return new ValidationResult("input invalid");
+	//		};
+			
+	//		return ValidationResult.Success;
+	//	}
+
+	//(object value)
+	//	{
+	//		return _inputHandler.IsInputValid(value.ToString());
+	//	}
+
+
+	}
+}
